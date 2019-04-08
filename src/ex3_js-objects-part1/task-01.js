7dc946f7a01b96959703b9a029c0a7fbd19f6956
Aleksandr Morozov <48330503+Santelecom@users.noreply.github.com>
2019-04-09 00:22:06 +0200

    Delete task-01.js

--- src/ex3_js-objects-part1/task-01.js
+++
@@ -1,8 +0,0 @@
-var objTypes = new Object();
-
-objTypes.typeSting = "text value";
-objTypes.typeNumber = 4;
-objTypes.typeBoolean = true;
-objTypes.typeArray = [1,4,2];
-
-delete objTypes.typeArray;
