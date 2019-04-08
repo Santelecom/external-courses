15aa6d6f8b532693d529a64413769017224a299d
Aleksandr Morozov <48330503+Santelecom@users.noreply.github.com>
2019-04-09 00:54:31 +0200

    Delete task-09.js

--- src/ex4_js-objects-part2/task-09.js
+++
@@ -1,9 +0,0 @@
-
-function insertString(strData, insertSrt, wordNum){
-	var result = strData.split(' ');
-	result.splice(wordNum + 1, 0, insertSrt);
-	return result.join(' ');
-}
-
-module.exports = insertString
-
